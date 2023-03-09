d0ff78a0d5b82d2e447dc31bcef9ef8e03321f9a
Rose Jang <jangmi749@gmail.com>
2023-03-09 15:29:34 +0900

    :lipstick: design : 기본 font 설정

--- client/src/GlobalStyle.tsx
+++ client/src/GlobalStyle.tsx
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR&display=swap');
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'IBM Plex Sans KR', sans-serif;
   }
 
   html, body, #root {
