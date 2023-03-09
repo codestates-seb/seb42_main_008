c52a85f66805dbedaf8cb629abde5f1188c00824
seungchul <seungchul0118@gmail.com>
2023-03-09 14:39:55 +0900

    :package: chore : 프로젝트 초기 설정

---
+++ server/src/main/java/partypeople/server/PartypeopleApplication.java
@@ -0,0 +1,13 @@
+package partypeople.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartypeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartypeopleApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/partypeople/server/exception/BusinessLogicException.java
@@ -0,0 +1,13 @@
+package partypeople.server.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException{
+    @Getter
+    private ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}
