93c5704248d708906765154bce0f93a41a048b82
es39 <esj10025@gmail.com>
2023-03-10 22:47:52 +0900

    :lipstick: design : ContentDetail page 디자인 수정

--- client/src/pages/ContentDetail.tsx
+++ client/src/pages/ContentDetail.tsx
@@ -1,5 +1,26 @@
+import styled from 'styled-components';
+
 const ContentDetail = () => {
-  return <div>contentDetail</div>;
+  return (
+    <Container>
+      <ContentBox></ContentBox>
+    </Container>
+  );
 };
 
 export default ContentDetail;
+
+const Container = styled.main`
+  background-color: #5d62a0;
+`;
+const ContentBox = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  width: 1280px;
+  height: 860px;
+  padding: 50px;
+  box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+`;
