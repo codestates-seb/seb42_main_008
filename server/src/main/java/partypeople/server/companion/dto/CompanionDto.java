ba61cfe1ce975cb7fb884fa2a17a520e8cb36d40
sugyeonghh <sugyeong.py@gmail.com>
2023-03-11 14:13:56 +0900

    :bug: fix : postDto 애너테이션 수정

--- server/src/main/java/partypeople/server/companion/dto/CompanionDto.java
+++ server/src/main/java/partypeople/server/companion/dto/CompanionDto.java
@@ -1,20 +1,25 @@
 package partypeople.server.companion.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.List;
 
 public class CompanionDto {
 
     @Getter
+    @AllArgsConstructor
     public static class Post {
         @NotBlank
         private String title;
 
+        @NotBlank
         private String content;
 
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -23,8 +28,10 @@ public class CompanionDto {
         @NotBlank
         private String address;
 
+        @NotNull
         private Double lat;
 
+        @NotNull
         private Double lng;
 
         @NotBlank
@@ -36,6 +43,7 @@ public class CompanionDto {
         @Positive
         private Integer continent;
 
+        @Size(min = 2, max = 5)
         private List<String> tags;
 
         @Positive
