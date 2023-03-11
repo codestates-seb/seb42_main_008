80801056ec73ab3908549de0cf34e81353829bb9
sugyeonghh <sugyeong.py@gmail.com>
2023-03-11 14:18:19 +0900

    :recycle: refactor : 동행글 등록 서비스 리팩터링

--- server/src/main/java/partypeople/server/companion/service/CompanionService.java
+++ server/src/main/java/partypeople/server/companion/service/CompanionService.java
@@ -5,18 +5,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import partypeople.server.companion.entity.Companion;
 import partypeople.server.companion.repository.CompanionRepository;
+import partypeople.server.exception.BusinessLogicException;
+import partypeople.server.exception.ExceptionCode;
+import partypeople.server.member.service.MemberService;
+import partypeople.server.nation.entity.Nation;
 import partypeople.server.nation.service.NationService;
+import partypeople.server.utils.CustomBeanUtils;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CompanionService {
     private final CompanionRepository companionRepository;
+    private final MemberService memberService;
     private final NationService nationService;
 
     public Companion createCompanion(Companion companion) {
-
-        companion.setNation(nationService.findNation(companion.getNation()));
+        memberService.findMember(companion.getMember().getMemberId());
+        Nation nation = nationService.findNation(companion.getNation());
+        companion.setNation(nation);
 
         return companionRepository.save(companion);
     }
