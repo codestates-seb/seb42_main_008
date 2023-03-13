57b9a7c9fde13800d08f74c20be5aa2a1f1b8c0e
Rose Jang <jangmi749@gmail.com>
2023-03-13 09:49:47 +0900

    :lipstick: design : Main SectionWrapper 반응형 속성 추가

--- client/src/components/Main/SectionWrapper.tsx
+++ client/src/components/Main/SectionWrapper.tsx
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 
 export const SectionWrapper = styled.section`
   width: 100%;
+  height: calc(100vh - 60px);
+
+  @media screen and (max-width: 992px) {
+    height: fit-content;
+  }
 `;
