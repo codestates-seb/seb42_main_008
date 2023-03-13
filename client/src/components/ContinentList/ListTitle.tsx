e815155f2d6939152de4a498533a252d1d8a2519
Rose Jang <jangmi749@gmail.com>
2023-03-13 16:12:33 +0900

    :lipstick: design : ContentList 제목, 사진부분 디자인 구현

---
+++ client/src/components/ContinentList/ListTitle.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import ImageFilter from 'components/Main/ImageFilter';
+import { useParams } from 'react-router-dom';
+import countries from 'assets/countries.json';
+
+interface CountryData {
+  name: string;
+  code: string;
+}
+
+interface CountryNames {
+  en: string;
+  ko: string;
+}
+
+const ListTitle = () => {
+  const { continent, countryCode } = useParams();
+  const countriesData: CountryData[] = countries[continent as keyof object];
+  const countryName = countriesData.filter(item => item.code === countryCode)[0]
+    .name;
+  const names: CountryNames = {
+    en: countryName.split('(')[1].slice(0, -1),
+    ko: countryName.split('(')[0].slice(0, -1),
+  };
+
+  return (
+    <ListTitleWrapper
+      style={{
+        backgroundImage: `url(https://source.unsplash.com/featured/?${names.en.replace(
+          /\s+/g,
+          ''
+        )},city)`,
+      }}
+    >
+      <TitleText>
+        <h1>{names.en.toUpperCase()}</h1>
+        <h2>{names.ko}</h2>
+      </TitleText>
+      <Filter></Filter>
+    </ListTitleWrapper>
+  );
+};
+
+const ListTitleWrapper = styled.div`
+  width: 100%;
+  height: 35vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+`;
+
+const TitleText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+  color: #fff;
+`;
+
+const Filter = styled(ImageFilter)`
+  opacity: 0.35;
+`;
+
+export default ListTitle;
--- client/src/pages/ContentList.tsx
+++ client/src/pages/ContentList.tsx
@@ -1,5 +1,30 @@
+import ListTitle from 'components/ContinentList/ListTitle';
+import styled from 'styled-components';
+
 const ContentList = () => {
-  return <div>ContentList</div>;
+  return (
+    <Container>
+      <ListTitle />
+      <SearchBox></SearchBox>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+const SearchBox = styled.div`
+  width: 80%;
+  height: 60px;
+  background-color: #feb35c;
+  position: absolute;
+  top: 32vh;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+`;
 
 export default ContentList;
