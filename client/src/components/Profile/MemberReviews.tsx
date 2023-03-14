6c2c0d457484dda355bfae28edae1ec8224ee298
Rose Jang <jangmi749@gmail.com>
2023-03-14 17:03:23 +0900

    :sparkles: feat : Emoji 컴포넌트 생성

--- client/src/components/Profile/MemberReviews.tsx
+++ client/src/components/Profile/MemberReviews.tsx
@@ -1,5 +1,35 @@
-const MemberReviews = () => {
-  return <div>Review</div>;
+import { BsEmojiSmile, BsEmojiNeutral, BsEmojiFrown } from 'react-icons/bs';
+
+import { Review } from 'interfaces/Profile.interface';
+
+interface ReviewProps {
+  reviews: Review[];
+}
+
+interface EmojiProps {
+  score: number;
+}
+
+const Emoji = ({ score }: EmojiProps) => {
+  if (score === -1) {
+    return <span className="sad-face">{<BsEmojiFrown />}</span>;
+  } else if (score === 0) {
+    return <span className="neutral-face">{<BsEmojiNeutral />}</span>;
+  }
+  return <span className="smile-face">{<BsEmojiSmile />}</span>;
+};
+
+const MemberReviews = ({ reviews }: ReviewProps) => {
+  console.log(reviews);
+  return (
+    <div>
+      {reviews.map((item, idx) => (
+        <div key={idx}>
+          <Emoji score={item.score} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MemberReviews;
