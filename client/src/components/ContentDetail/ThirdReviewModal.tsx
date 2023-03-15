a622c7ce6cbb875563a5d8a04c077ea26e5e3cdc
es39 <esj10025@gmail.com>
2023-03-15 15:19:36 +0900

    :lipstick: design : modal 반응형 수정

--- client/src/components/ContentDetail/ThirdReviewModal.tsx
+++ client/src/components/ContentDetail/ThirdReviewModal.tsx
@@ -62,11 +62,41 @@ const ModalView = styled.div`
   justify-content: center;
   align-items: center;
   background-color: white;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   width: 50%;
   height: 40%;
   text-align: center;
   border-radius: 30px;
   box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.25);
+  @media screen and (max-width: 992px) {
+    width: 500px;
+    height: 300px;
+    font-size: 20px;
+    .btn-wrapper {
+      button {
+        width: 80%;
+        font-size: 15px;
+      }
+    }
+  }
+  @media screen and (max-width: 768px) {
+    width: 500px;
+    height: 300px;
+  }
+  @media screen and (max-width: 576px) {
+    width: 300px;
+    height: 300px;
+    font-size: 15px;
+    .btn-wrapper {
+      button {
+        width: 90%;
+        font-size: 15px;
+      }
+    }
+  }
 `;
 const Score = styled.div`
   display: flex;
