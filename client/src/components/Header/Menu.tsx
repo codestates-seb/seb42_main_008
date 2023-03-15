a901eeb38d46bde2ed78c7e255d1476be5b1d2db
김상교 <sangyoes@gimsang-gyoui-MacBookPro.local>
2023-03-15 14:55:59 +0900

    :lipstick: design : 헤더 반응형 구현

---
+++ client/src/components/Header/Menu.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+type Props = {
+  setIsShowMenu: (newValue: boolean) => void;
+  setIsLogin: (newValue: boolean) => void;
+};
+const Menu = ({ setIsShowMenu, setIsLogin }: Props) => {
+  const handleMenuEnter = () => {
+    setIsShowMenu(true);
+  };
+  const handleMenuLeave = () => {
+    setIsShowMenu(false);
+  };
+  const handleLogout = () => {
+    setIsLogin(false);
+  };
+
+  return (
+    <MenuBox onMouseEnter={handleMenuEnter} onMouseLeave={handleMenuLeave}>
+      <Link to="/:memberId/profile">Profile</Link>
+      <Link to="/continents">파티 구하기</Link>
+      <div onClick={handleLogout}>Logout</div>
+    </MenuBox>
+  );
+};
+
+export default Menu;
+
+const MenuBox = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: white;
+  color: black;
+  top: 60px;
+  right: 0;
+  width: 150px;
+  height: 150px;
+  position: fixed;
+  border-bottom: 1px solid #5d62a0;
+  border-left: 1px solid #5d62a0;
+  > a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    color: black;
+    border-bottom: 1px solid #5d62a0;
+    &:hover {
+      background-color: #feb35c;
+      color: white;
+    }
+  }
+  > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    &:hover {
+      background-color: #feb35c;
+      color: white;
+    }
+  }
+`;
