c5c010c558f36d22641bc06d79a6b30b61178e62
Rose Jang <jangmi749@gmail.com>
2023-03-15 14:32:58 +0900

    :lipstick: design : Profile 페이지 배경이미지 변경

--- client/src/components/Profile/BackGroundImage.tsx
+++ client/src/components/Profile/BackGroundImage.tsx
@@ -3,14 +3,10 @@ import ImageFilter from 'components/Main/ImageFilter';
 import styled from 'styled-components';
 
 const BackGroundImage = () => {
-  const getRandomImg = () => {
-    const randomIdx = Math.floor(Math.random() * 3);
-    return imgLinks[randomIdx];
-  };
   return (
     <BgWrapper>
       <ImageFilter></ImageFilter>
-      <img src={getRandomImg()} alt="profileBackground" />
+      <img src={imgLinks[2]} alt="profileBackground" />
     </BgWrapper>
   );
 };
