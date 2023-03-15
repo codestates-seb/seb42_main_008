26c3f73252fa98fb2ecdb78561b4f9030c46b89d
Rose Jang <jangmi749@gmail.com>
2023-03-15 10:37:56 +0900

    :sparkles: feat : 모달 띄울 시 부모요소 스크롤 없애기

--- client/src/components/Profile/FollowModal.tsx
+++ client/src/components/Profile/FollowModal.tsx
@@ -1,7 +1,28 @@
-import { FollowModalProps } from 'interfaces/Profile.interface';
+import { Follow, FollowModalProps } from 'interfaces/Profile.interface';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import memberData from 'profileTestData.json';
 
 const FollowModal = ({ setIsShowModal, isFollower }: FollowModalProps) => {
+  const [followerList, setFollowerList] = useState<Follow[] | []>([]);
+  const [followingList, setFollowingList] = useState<Follow[] | []>([]);
+
+  useEffect(() => {
+    setFollowerList(memberData.follows);
+    setFollowingList(memberData.follows);
+
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
+
   return (
     <>
       <ModalBG
@@ -9,7 +30,34 @@ const FollowModal = ({ setIsShowModal, isFollower }: FollowModalProps) => {
         onClick={() => setIsShowModal(false)}
       ></ModalBG>
       <ModalContent>
-        <h1>{isFollower ? '팔로워' : '팔로잉'}</h1>
+        <div>
+          <h1>{isFollower ? '팔로워' : '팔로잉'}</h1>
+        </div>
+        {isFollower ? (
+          <FollowList>
+            {followerList.map(follower => (
+              <FollowUser key={follower.memberID}>
+                <img
+                  src={follower.profile}
+                  alt={follower.nickname + 'profile'}
+                />
+                <span>{follower.nickname}</span>
+              </FollowUser>
+            ))}
+          </FollowList>
+        ) : (
+          <FollowList>
+            {followingList.map(following => (
+              <FollowUser key={following.memberID}>
+                <img
+                  src={following.profile}
+                  alt={following.nickname + 'profile'}
+                />
+                <span>{following.nickname}</span>
+              </FollowUser>
+            ))}
+          </FollowList>
+        )}
       </ModalContent>
     </>
   );
@@ -24,6 +72,7 @@ const ModalBG = styled.div`
   z-index: 999;
   top: 0;
   left: 0;
+  overflow: hidden;
 `;
 
 const ModalContent = styled.div`
@@ -38,6 +87,43 @@ const ModalContent = styled.div`
   transform: translate(-50%, -50%);
   z-index: 1000;
   padding: 30px;
+
+  > div {
+    margin-bottom: 20px;
+    > h1 {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media screen and (max-width: 576px) {
+    width: 80%;
+  }
+`;
+
+const FollowList = styled.ul`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const FollowUser = styled.li`
+  width: 100%;
+  height: 50px;
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  > img {
+    width: 40px;
+    height: 40px;
+    object-fit: cover;
+    object-position: center;
+    border-radius: 50%;
+  }
 `;
 
 export default FollowModal;
--- client/src/components/Profile/MemberInfo.tsx
+++ client/src/components/Profile/MemberInfo.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-
 import { TbGenderFemale, TbGenderMale, TbMail } from 'react-icons/tb';
 import { SlUserFollow, SlUserFollowing } from 'react-icons/sl';
 import memberData from 'profileTestData.json';
