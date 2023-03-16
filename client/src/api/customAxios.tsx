302e26a3d5cf5bac4e8aee86aebdcc8bc5ae7aba
Rose Jang <jangmi749@gmail.com>
2023-03-16 10:44:52 +0900

    :sparkles: feat : custom axios 설정

--- client/src/api/customAxios.tsx
+++ client/src/api/customAxios.tsx
@@ -1,7 +1,12 @@
 import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 const customAxios = axios.create({
   baseURL: 'http://localhost:3001',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
 });
 
 export default customAxios;
