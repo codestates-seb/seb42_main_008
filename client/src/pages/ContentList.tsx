1a5f987b08f4c5dc274b7975f63b9c239111a486
Rose Jang <jangmi749@gmail.com>
2023-03-16 09:45:00 +0900

    :sparkles: feat : window 사이즈별 pagesize 변경

--- client/src/pages/ContentList.tsx
+++ client/src/pages/ContentList.tsx
@@ -1,27 +1,69 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { ListData, SortBy } from 'interfaces/ContentList.interface';
+import {
+  ListData,
+  ListQueryString,
+  SortBy,
+} from 'interfaces/ContentList.interface';
 import ListTitle from 'components/ContinentList/ListTitle';
 import ListSearch from 'components/ContinentList/ListSearch';
 import ListItems from 'components/ContinentList/ListItems';
 import customAxios from 'api/customAxios';
+import { useParams } from 'react-router-dom';
 
 const ContentList = () => {
+  const { countryCode } = useParams();
   const [datas, setDatas] = useState<ListData[] | []>([]);
   const [sortData, setSortData] = useState<SortBy>({
     value: '작성날짜 (최신순)',
     sortBy: 'createdAt',
     sortDir: 'DESC',
   });
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(12);
 
   const getContentData = async () => {
+    const params: ListQueryString = {
+      page,
+      size,
+      sortDir: sortData.sortDir,
+      sortBy: sortData.sortBy,
+      nationCode: countryCode,
+    };
+
     await customAxios.get('/companions').then(resp => {
       setDatas(resp.data);
     });
+
+    await customAxios
+      .get('/companions/nations', {
+        params,
+      })
+      .then(resp => {
+        console.log(resp.data);
+        setPage(1);
+        setSize(12);
+      });
+  };
+
+  const handleSize = () => {
+    const width = window.innerWidth;
+    if (width < 620) {
+      setSize(3);
+    } else if (width < 922) {
+      setSize(6);
+    } else if (width < 1200) {
+      setSize(9);
+    } else {
+      setSize(12);
+    }
   };
 
   useEffect(() => {
     getContentData();
+    window.addEventListener('resize', handleSize);
+    handleSize();
+    return () => window.removeEventListener('resize', handleSize);
   }, [sortData]);
 
   return (
