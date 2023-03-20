9ea77e66c9c65d98d1b4cc53a3c08c6178eaa5fd
김상교 <sangyoes@gimsang-gyoui-MacBookPro.local>
2023-03-20 17:41:05 +0900

    :sparkles: feat : 쪽지 상태 변경 및 안읽은쪽지 개수구현

--- client/src/api/customAxios.tsx
+++ client/src/api/customAxios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const customAxios = axios.create({
-  baseURL: 'https://86de-1-237-37-135.jp.ngrok.io',
+  baseURL: 'https://ad9b-59-10-231-15.jp.ngrok.io',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*',
