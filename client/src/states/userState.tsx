a48fb0a75759aa422ddc4073314035dcb830f91c
es39 <esj10025@gmail.com>
2023-03-21 20:34:26 +0900

    :sparkles: feat : cookie add

--- client/src/states/userState.tsx
+++ client/src/states/userState.tsx
@@ -3,34 +3,14 @@ import { recoilPersist } from 'recoil-persist';
 
 const { persistAtom } = recoilPersist();
 
-// 로그인 상태
+// * 로그인 상태
 export const loginState = atom({
   key: 'loginState',
   default: false,
   effects_UNSTABLE: [persistAtom],
 });
 
-// 로그인 토큰 정보
-export const userToken = atom({
-  key: 'userToken',
-  default: { authorization: '' },
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const userRefreshToken = atom({
-  key: 'userRefreshToken',
-  default: '',
-  effects_UNSTABLE: [persistAtom],
-});
-
-// 로그인 토큰 디코딩 정보
-export const userDecodeToken = atom({
-  key: 'userTokenLoading',
-  default: false,
-  effects_UNSTABLE: [persistAtom],
-});
-
-// 로그인 유저 정보
+// * 로그인 유저 정보
 export const userInfo = atom({
   key: 'userInfo',
   default: {
@@ -42,17 +22,8 @@ export const userInfo = atom({
     email: '',
     memberId: 0,
     sub: '',
-    iat: 1679033173,
-    exp: 1679033773,
+    iat: 0,
+    exp: 0,
   },
   effects_UNSTABLE: [persistAtom],
 });
-
-// 리뷰 작성 정보
-export const reviewInfo = atom({
-  key: 'reviewInfo',
-  default: false,
-  effects_UNSTABLE: [persistAtom],
-});
-
-// export type { userInfoTypes, userInfoType };
