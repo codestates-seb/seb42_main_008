70018f502eecf624be0ff0708c5ef018dd6e27e6
seungchul <seungchul0118@gmail.com>
2023-03-22 15:06:35 +0900

    :package: chore : deploy.sh 수정(./bashrc 실행 코드 추가)

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -14,6 +14,8 @@ cp $BUILD_JAR $DEPLOY_PATH
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
+source /home/ubuntu/.bashrc
+
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
@@ -26,5 +28,6 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-source /home/ubuntu/.bashrc
+source /home/ubuntu/.bashprofile
+source /home/ssm-user/.bashrc
 sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=server >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
