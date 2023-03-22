7af8450c5946a217696a04f04dcc64677e1da73e
seungchul <seungchul0118@gmail.com>
2023-03-22 17:00:05 +0900

    Update deploy.sh

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -27,6 +27,6 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-source /home/ubuntu/.bashrc
+source /home/ubuntu/.bashprofile
 printenv >> /home/ubuntu/action/deploy.log
 nohup java -jar $DEPLOY_JAR --spring.profiles.active=server > /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
