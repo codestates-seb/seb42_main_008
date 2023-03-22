7e05567a103fc49c50837f4373297e6776dccd50
seungchul <seungchul0118@gmail.com>
2023-03-22 13:58:02 +0900

    :package: chore : deploy.sh 수정(./bashrc 실행 코드 추가)

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -26,4 +26,5 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
+source ~/.bashrc
 sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=server >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
