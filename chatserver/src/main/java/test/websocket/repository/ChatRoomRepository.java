65cee2026764efc60c9bb163236a60a6c4f2a8ce
JoeGukseon <zipcks1381@gmail.com>
2023-05-25 17:12:02 +0900

    :sparkles: feat : 동행글 초기화 가져오기 업데이트

--- chatserver/src/main/java/test/websocket/repository/ChatRoomRepository.java
+++ chatserver/src/main/java/test/websocket/repository/ChatRoomRepository.java
@@ -1,21 +1,29 @@
 package test.websocket.repository;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import test.websocket.dto.ChatRoomDTO;
 import test.websocket.dto.CompanionChatDTO;
+import test.websocket.service.WebServerService;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
+@RequiredArgsConstructor
 public class ChatRoomRepository {
 
     private Map<String, ChatRoomDTO> chatRoomDTOMap;
-
+    private final WebServerService webServerService;
     @PostConstruct
     private void init(){
         chatRoomDTOMap = new ConcurrentHashMap<>();
+        //동행글번호 가져오기 (미완료된 동행글번호만) 웹서버에다가 >
+        List<CompanionChatDTO> companionChatDTOS = webServerService.getInCompleteNumbers();
+        for (CompanionChatDTO companion : companionChatDTOS) {
+            createChatRoomDTO(companion);
+        }
     }
 
     public List<ChatRoomDTO> findAllRooms(){
