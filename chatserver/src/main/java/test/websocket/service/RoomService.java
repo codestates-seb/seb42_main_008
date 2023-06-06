e31df7baf2515978e6c0a4cd694f884e305fce2d
JoeGukseon <zipcks1381@gmail.com>
2023-06-07 01:34:25 +0900

    :recycle: refactor : RoomService 수정 (메시지저장시 유저리스트 추가, 체크리스트 삭제, 읽지않은 메시지 수, 마지막 체크 타임 추가)

--- chatserver/src/main/java/test/websocket/service/RoomService.java
+++ chatserver/src/main/java/test/websocket/service/RoomService.java
@@ -3,15 +3,17 @@ package test.websocket.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import test.websocket.dto.ChatData;
 import test.websocket.dto.ChatRoom;
 import test.websocket.dto.CompanionChatDTO;
 import test.websocket.dto.ChatUser;
 import test.websocket.repository.MongoDBRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -28,10 +30,36 @@ public class RoomService {
                 }).subscribe();
     }
 
+//    public Mono<Void> insertMsg(ChatData chatData, String roomId) {
+//        return mongoDBRepository.findByRoomId(roomId)
+//                .publishOn(Schedulers.boundedElastic())
+//                .flatMap(c -> mongoDBRepository.findUsersByRoomId(roomId)
+//                        .map(s -> s.getUsers().stream().map(
+//                                ChatUser::getEmail
+//                        ).collect(Collectors.toList()))
+//                        .flatMap(names -> {
+//                            names.remove(chatData.getEmail());
+//                            chatData.setCheckList(names);
+//                            return mongoDBRepository.pushMessage(roomId, chatData);
+//                        }))
+//                .doOnError(e -> log.info("message error: {}", e.getMessage()))
+//                .then();
+//    }
     public Mono<Void> insertMsg(ChatData chatData, String roomId) {
+        /* 메세지에 유저리스트 넣는 부분인데 여기도 디비에 유저정보 요청이 들어감 수정필요 */
         return mongoDBRepository.findByRoomId(roomId)
-                .then(mongoDBRepository.pushMessage(roomId, chatData))
-                .doOnError(e -> log.info("message error: {}", e.getMessage()));
+                .publishOn(Schedulers.boundedElastic())
+                .flatMap(chatRoom -> mongoDBRepository.findUsersByRoomId(roomId)
+                        .map(users -> users.getUsers().stream()
+                                .map(ChatUser::getEmail)
+                                .filter(email -> !email.equals(chatData.getEmail()))
+                                .collect(Collectors.toList()))
+                        .flatMap(names -> {
+                            chatData.setCheckList(names);
+                            return mongoDBRepository.pushMessage(roomId, chatData);
+                        }))
+                .doOnError(e -> log.info("message error: {}", e.getMessage()))
+                .then();
     }
 
     public Mono<Void> saveUser(ChatUser user, String roomId) {
@@ -46,5 +74,25 @@ public class RoomService {
 
     public Mono<ChatRoom> findRoomByRoomId(String roomId) {
         return mongoDBRepository.findByRoomId(roomId);
+    }
+
+    public Mono<Void> deleteCheckListByEmail(String roomId,String email,String chatDataId) {
+        return mongoDBRepository.deleteCheckList(roomId, email, chatDataId);
+    }
+
+
+    public Mono<Integer> findNotReadMessageCount(String roomId, String email) {
+        /* lasttime을 어떻게 가져오지? */
+        return mongoDBRepository.findMessagesByRoomId(roomId, LocalDateTime.now().minusHours(10))
+                .map(chatRoom -> {
+                    List<ChatData> messages = chatRoom.getMessages();
+                    return (int) messages.stream()
+                            .filter(message -> message.getCheckList().contains(email))
+                            .count();
+                });
+    }
+
+    public Mono<Void> updateLastTime(String roomId, String email) {
+        return mongoDBRepository.updateLastTime(roomId,email);
     }
 }
