e3ae469c74d118251cdd124ea030c681ca63e7c3
JoeGukseon <zipcks1381@gmail.com>
2023-06-06 17:09:57 +0900

    :recycle: refactor : ChatDTO 수정 (기본생성자 추가, 생성자 추가)

--- chatserver/src/main/java/test/websocket/dto/ChatDTO.java
+++ chatserver/src/main/java/test/websocket/dto/ChatDTO.java
@@ -1,12 +1,15 @@
 package test.websocket.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatDTO {
     private String roomId;
     private String nickname;
@@ -14,4 +17,13 @@ public class ChatDTO {
     private String profile;
     private String message;
     private LocalDateTime curTime;
+
+    public ChatDTO(ChatData chatData, String roomId) {
+        this.roomId = roomId;
+        this.nickname = chatData.getNickname();
+        this.email = chatData.getEmail();
+        this.profile = chatData.getProfile();
+        this.message = chatData.getMessage();
+        this.curTime = chatData.getCurTime();
+    }
 }
