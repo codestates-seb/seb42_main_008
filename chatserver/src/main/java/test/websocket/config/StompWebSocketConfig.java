631bc23bda2c74a4cbaf2db061636e202f8f77e8
JoeGukseon <zipcks1381@gmail.com>
2023-06-06 17:16:39 +0900

    :recycle: refactor : StompWebSocketConfig 수정 (setAllowedOriginPatterns CORS 설정)

--- chatserver/src/main/java/test/websocket/config/StompWebSocketConfig.java
+++ chatserver/src/main/java/test/websocket/config/StompWebSocketConfig.java
@@ -11,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOriginPatterns("http://localhost:3000","*")
+//                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
