b6c4d45122642d3f17f6b30a9b9ba187e7090814
JoeGukseon <zipcks1381@gmail.com>
2023-06-07 01:29:36 +0900

    :recycle: refactor : ChatUser 수정 (마지막 활동시간 필드 추가)

--- chatserver/src/main/java/test/websocket/dto/ChatUser.java
+++ chatserver/src/main/java/test/websocket/dto/ChatUser.java
@@ -3,7 +3,9 @@ package test.websocket.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -11,10 +13,12 @@ public class ChatUser {
     private String email;
     private String profile;
     private String nickname;
+    private LocalDateTime lastCheckTime;
 
     public ChatUser(ChatDTO chatDTO) {
         this.email = chatDTO.getEmail();
         this.nickname = chatDTO.getNickname();
         this.profile = chatDTO.getProfile();
+        this.lastCheckTime = LocalDateTime.now();
     }
 }
