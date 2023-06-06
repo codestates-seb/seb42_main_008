a945eeeabdcf49a72d371b92c9a64d6e415e36cb
JoeGukseon <zipcks1381@gmail.com>
2023-06-07 01:27:45 +0900

    :recycle: refactor : ChatData 수정 (읽지않은 유저 구분위해 ID 추가)

--- chatserver/src/main/java/test/websocket/dto/ChatData.java
+++ chatserver/src/main/java/test/websocket/dto/ChatData.java
@@ -3,19 +3,30 @@ package test.websocket.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatData {
+    private String id;
     private String nickname;
     private String email;
     private String profile;
     private String message;
     private LocalDateTime curTime;
+    private List<String> checkList = new ArrayList<>();
 
     public ChatData(ChatDTO chatDTO) {
+        this.id = UUID.randomUUID().toString();
         this.email = chatDTO.getEmail();
         this.message = chatDTO.getMessage();
         this.nickname = chatDTO.getNickname();
