ec81cd2da38450c962f1b262fea8b90f07aff024
JoeGukseon <zipcks1381@gmail.com>
2023-06-06 17:15:09 +0900

    :recycle: refactor : WebsocketApplication 수정

--- chatserver/src/main/java/test/websocket/repository/MongoDBRepository.java
+++ chatserver/src/main/java/test/websocket/repository/MongoDBRepository.java
@@ -1,9 +1,20 @@
 package test.websocket.repository;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import test.websocket.dto.ChatRoom;
 
+import java.util.List;
+
+
 public interface MongoDBRepository extends ReactiveMongoRepository<ChatRoom, String>, MongoDBRepositoryCustom {
     Mono<ChatRoom> findByRoomId(String roomId);
+
+    Flux<ChatRoom> findByUsersEmailOrderByLastTimeDesc(String email);
+
+//    @Query("{'users.email': ?0}")
+//    Mono<List<ChatRoom>> findByUsersContainingOrderByLastTimeDesc(String email);
+//    Flux<ChatRoom> findByUsersContaining(String email);
 }
