b154fed771218b12721a8d0332d3bbf5a0789e0b
JoeGukseon <zipcks1381@gmail.com>
2023-06-08 22:49:47 +0900

    :recycle: Refactor : MongoConfig 수정 (트랜잭션 테스트)

--- chatserver/src/main/java/test/websocket/config/MongoConfig.java
+++ chatserver/src/main/java/test/websocket/config/MongoConfig.java
@@ -3,12 +3,34 @@ package test.websocket.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.SessionSynchronization;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 @Configuration
-public class MongoConfig {
+public class MongoConfig  {
+    //public class MongoConfig extends AbstractMongoClientConfiguration {
+//    private String dbName = "mongotest";
+//    private String dbUrl = "http://localhost:27017/";
+//    @Bean
+//    public ReactiveMongoTransactionManager transactionManager(ReactiveMongoDatabaseFactory dbFactory) {
+//        return new ReactiveMongoTransactionManager(dbFactory);
+//    }
     @Bean
     public ReactiveMongoTemplate reactiveMongoTemplate(ReactiveMongoDatabaseFactory factory) {
-        return new ReactiveMongoTemplate(factory);
+        ReactiveMongoTemplate mongoTemplate = new ReactiveMongoTemplate(factory);
+        mongoTemplate.setSessionSynchronization(SessionSynchronization.ALWAYS);
+        return mongoTemplate;
     }
+
+//    @Override
+//    public MongoClient mongoClient() {
+//        ConnectionString connectionString = new ConnectionString(dbUrl);
+//        MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
+//        return MongoClients.create(mongoClientSettings);
+//    }
+
+//    @Override
+//    protected String getDatabaseName() {
+//        return dbName;
+//    }
 }
