a7101abcf5e399791dd90046ab76725149e8f6d7
JoeGukseon <zipcks1381@gmail.com>
2023-06-10 10:10:05 +0900

    :recycle: refactor : StompChatController 수정 - 비동기 타이머 추가

--- chatserver/src/main/java/test/websocket/controller/StompChatController.java
+++ chatserver/src/main/java/test/websocket/controller/StompChatController.java
@@ -1,20 +1,19 @@
 package test.websocket.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import test.websocket.annotation.MeasureExecutionTime;
 import test.websocket.dto.ChatData;
 import test.websocket.dto.ChatDTO;
 import test.websocket.dto.ChatUser;
 import test.websocket.service.RoomService;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Controller
@@ -34,11 +33,17 @@ public class StompChatController {
     }
 
     @MessageMapping(value = "/chat/message")
+//    @MeasureExecutionTime
     public Mono<Void> message(ChatDTO message) {
+        Instant startTime = Instant.now();
         return Mono.just(message).flatMap(m -> {
             m.setCurTime(LocalDateTime.now());
             template.convertAndSend("/sub/chat/room/" + m.getRoomId(), m);
-            return roomService.insertMsg(new ChatData(m), m.getRoomId());
+            return roomService.insertMsg(new ChatData(m), m.getRoomId())
+                    .doOnSuccess(v -> {
+                        Duration duration = Duration.between(startTime, Instant.now());
+                        System.out.println("Processing time: " + duration.toMillis() + " milliseconds");
+                    });
         });
     }
 
@@ -48,9 +53,14 @@ public class StompChatController {
 //                .then();
 //    }
 
+//    @MessageMapping(value = "/chat/check")
+//    public Mono<Void> checkMessage(@RequestBody ChatDTO.Check check) {
+//        return roomService.deleteCheckListByEmail(check.getRoomId(), check.getEmail(), check.getChatDataId())
+//                .then(Mono.defer(() -> roomService.updateLastTime(check.getRoomId(), check.getEmail())));
+//    }
     @MessageMapping(value = "/chat/check")
     public Mono<Void> checkMessage(@RequestBody ChatDTO.Check check) {
-        return roomService.deleteCheckListByEmail(check.getRoomId(), check.getEmail(), check.getChatDataId())
-                .then(Mono.defer(() -> roomService.updateLastTime(check.getRoomId(), check.getEmail())));
+
+        return roomService.updateLastTime(check.getRoomId(), check.getEmail());
     }
 }
