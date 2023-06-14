b68b9419f83b3304a738c43a95f7e6122e54c27b
JoeGukseon <zipcks1381@gmail.com>
2023-06-14 23:45:55 +0900

    :recycle: refactor : MongoDBRepositoryCustom 수정 - findNotReadMessagesByRoomId 추가

--- chatserver/src/main/java/test/websocket/repository/MongoDBRepositoryCustom.java
+++ chatserver/src/main/java/test/websocket/repository/MongoDBRepositoryCustom.java
@@ -4,10 +4,14 @@ import reactor.core.publisher.Mono;
 import test.websocket.dto.ChatData;
 import test.websocket.dto.ChatUser;
 
+import java.time.LocalDateTime;
+
 public interface MongoDBRepositoryCustom {
     Mono<Void> pushMessage(String roomId, ChatData chatData);
     Mono<Void> pushUser(String roomId, ChatUser user);
     Mono<Void> deleteCheckList(String roomId, String email, String uuid);
 
     Mono<Void> updateLastTime(String roomId,String email);
+
+    Mono<Integer> findNotReadMessagesByRoomId(String roomId, LocalDateTime afterTime);
 }
