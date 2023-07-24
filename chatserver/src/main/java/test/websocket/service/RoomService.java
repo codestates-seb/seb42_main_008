c7a1805e770f35f6e46074e27f818f2792e68a5f
JoeGukseon <zipcks1381@gmail.com>
2023-07-24 13:40:52 +0900

    :recycle: refactor : RoomService 코드 정리 주석제거

--- chatserver/src/main/java/test/websocket/service/RoomService.java
+++ chatserver/src/main/java/test/websocket/service/RoomService.java
@@ -7,8 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 import test.websocket.dto.ChatData;
 import test.websocket.dto.ChatRoom;
-import test.websocket.dto.CompanionChatDTO;
 import test.websocket.dto.ChatUser;
+import test.websocket.dto.CompanionChatDTO;
 import test.websocket.repository.MongoDBRepository;
 
 import javax.annotation.PostConstruct;
@@ -22,67 +22,21 @@ import java.util.List;
 public class RoomService {
     private final MongoDBRepository mongoDBRepository;
 
-    //    @Transactional
-//    public Mono<Void> createRoom(Mono<CompanionChatDTO> requestBodyMono) {
-//        return requestBodyMono
-//                .flatMap(dto -> {
-//                    ChatRoom room = new ChatRoom(dto.getCompanionId(), dto.getCompanionTitle());
-//                    return mongoDBRepository.save(room).flatMap(savedRoom -> {
-//                        // 익셉션 발생 시키기 (예시로 RuntimeException 사용)
-//                        throw new RuntimeException("Rollback test");
-//                    });
-//                })
-//                .then();
-//    }
     @PostConstruct
     public void init() {
         mongoDBRepository.findNotReadMessagesByRoomId("1", LocalDateTime.now()).subscribe();
     }
+
     @Transactional
     public Mono<Void> createRoom(Mono<CompanionChatDTO> requestBodyMono) {
         return requestBodyMono
                 .flatMap(dto -> {
                     ChatRoom room = new ChatRoom(dto.getCompanionId(), dto.getCompanionTitle());
                     return mongoDBRepository.save(room);
-//                            .flatMap(savedRoom -> {
-//                                // 익셉션 발생 시키기 (예시로 RuntimeException 사용)
-//                                throw new RuntimeException("Rollback test");
-//                            });
                 })
                 .then();
     }
 
-//    public Mono<Void> insertMsg(ChatData chatData, String roomId) {
-//        return mongoDBRepository.findByRoomId(roomId)
-//                .publishOn(Schedulers.boundedElastic())
-//                .flatMap(c -> mongoDBRepository.findUsersByRoomId(roomId)
-//                        .map(s -> s.getUsers().stream().map(
-//                                ChatUser::getEmail
-//                        ).collect(Collectors.toList()))
-//                        .flatMap(names -> {
-//                            names.remove(chatData.getEmail());
-//                            chatData.setCheckList(names);
-//                            return mongoDBRepository.pushMessage(roomId, chatData);
-//                        }))
-//                .doOnError(e -> log.info("message error: {}", e.getMessage()))
-//                .then();
-//    }
-//    @Transactional
-//    public Mono<Void> insertMsg(ChatData chatData, String roomId) {
-//        /* 메세지에 유저리스트 넣는 부분인데 여기도 디비에 유저정보 요청이 들어감 수정필요 */
-//        return mongoDBRepository.findByRoomId(roomId)
-//                .publishOn(Schedulers.boundedElastic())
-//                .flatMap(chatRoom -> mongoDBRepository.findUsersByRoomId(roomId)
-//                        .map(users -> users.getUsers().stream()
-//                                .map(ChatUser::getEmail)
-//                                .filter(email -> !email.equals(chatData.getEmail()))
-//                                .collect(Collectors.toList()))
-//                        .flatMap(names -> {
-//                            chatData.setCheckList(names);
-//                            return mongoDBRepository.pushMessage(roomId, chatData);
-//                        }))
-//                .doOnError(e -> log.info("message error: {}", e.getMessage()));
-//    }
     public Mono<Void> insertMsg(ChatData chatData, String roomId) {
         return mongoDBRepository.pushMessage(roomId, chatData)
                 .doOnError(e -> log.info("message error: {}", e.getMessage()));
@@ -102,36 +56,7 @@ public class RoomService {
         return mongoDBRepository.findByRoomId(roomId);
     }
 
-//    public Mono<Void> deleteCheckListByEmail(String roomId,String email,String chatDataId) {
-//        return mongoDBRepository.deleteCheckList(roomId, email, chatDataId);
-//    }
-
-
-//    public Mono<Integer> findNotReadMessageCount(String roomId, String email) {
-//        /* lasttime을 어떻게 가져오지? */
-//        return mongoDBRepository.findMessagesByRoomId(roomId, LocalDateTime.now().minusHours(10))
-//                .map(chatRoom -> {
-//                    List<ChatData> messages = chatRoom.getMessages();
-//                    return (int) messages.stream()
-//                            .filter(message -> message.getCheckList().contains(email))
-//                            .count();
-//                });
-//    }
-
-//    public Mono<Integer> findNotReadMessageCount(String roomId, String email) {
-//        return mongoDBRepository.findMessagesByRoomId(roomId, mongoDBRepository.findUsersByRoomId(roomId)
-//                        .map(users -> users.getUsers().stream()
-//                        .filter(user -> user.getEmail().equals(email)))
-//                        .map((Stream<ChatUser> t) -> ChatUser.getLastCheckTime(t))
-//                        .next()
-//                )
-//                .map(chatRoom -> {
-//                    List<ChatData> messages = chatRoom.getMessages();
-//                    return messages.size();
-//                });
-//    }
     public Mono<Integer> findNotReadMessageCount(String roomId, String email) {
-        long startTime = System.currentTimeMillis();
         return mongoDBRepository.findUsersByRoomId(roomId)
                 .flatMap(chatRoom -> {
                     List<ChatUser> users = chatRoom.getUsers();
@@ -147,17 +72,12 @@ public class RoomService {
                     LocalDateTime lastTime = user.getLastCheckTime();
                     System.out.println("lastTime = " + lastTime);
                     return mongoDBRepository.findNotReadMessagesByRoomId(roomId, lastTime);
-                })
-                .doOnSuccess(v -> {
-                    long endTime = System.currentTimeMillis();
-                    long executionTime = endTime - startTime;
-                    System.out.println("Execution time: " + executionTime + "ms");
                 });
     }
 
 
     public Mono<Void> updateLastTime(String roomId, String email) {
-        return mongoDBRepository.updateLastTime(roomId,email);
+        return mongoDBRepository.updateLastTime(roomId, email);
     }
 
 }
