b0fb01c2501a98b9e38aae072f3cb1b0ae671d43
JoeGukseon <zipcks1381@gmail.com>
2023-07-24 13:39:47 +0900

    :recycle: refactor : MongoDBRepository 코드 정리 주석제거

--- chatserver/src/main/java/test/websocket/repository/MongoDBRepository.java
+++ chatserver/src/main/java/test/websocket/repository/MongoDBRepository.java
@@ -1,15 +1,10 @@
 package test.websocket.repository;
 
-import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import test.websocket.dto.ChatRoom;
-import test.websocket.dto.ChatUser;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 
 public interface MongoDBRepository extends ReactiveMongoRepository<ChatRoom, String>, MongoDBRepositoryCustom {
@@ -19,16 +14,4 @@ public interface MongoDBRepository extends ReactiveMongoRepository<ChatRoom, Str
 
     @Query(value = "{ '_id': ?0 }", fields = "{ 'users': 1, '_id': 0 }")
     Mono<ChatRoom> findUsersByRoomId(String roomId);
-
-//    @Query(value = "{ '_id': ?0 }", fields = "{ 'messages': 1, '_id': 0 }")
-//    Mono<ChatRoom> findMessagesByRoomId(String roomId);
-
-//    @Query(value = "{ '_id': ?0, 'messages.curTime': { $gt: ?1 } }", fields = "{ 'messages': 1, '_id': 0 }")
-//    Mono<ChatRoom> findMessagesByRoomId(String roomId, LocalDateTime afterTime);
-
-//    @Query(value = "{ '_id': ?0, 'messages.curTime': { $gt: ?1 } }", fields = "{ 'messages': 1, 'users':1,'_id': 0 }")
-//    Mono<ChatRoom> findMessagesByRoomId(String roomId, LocalDateTime afterTime);
-//    @Query("{'users.email': ?0}")
-//    Mono<List<ChatRoom>> findByUsersContainingOrderByLastTimeDesc(String email);
-//    Flux<ChatRoom> findByUsersContaining(String email);
 }
