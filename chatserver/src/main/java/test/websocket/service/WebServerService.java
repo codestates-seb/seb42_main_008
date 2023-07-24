d28ea88c5bc90c12ac3c566d3bd70dc076ed99f0
JoeGukseon <zipcks1381@gmail.com>
2023-07-24 13:42:46 +0900

    :recycle: refactor : WebServerService 코드 정리 주석제거

--- chatserver/src/main/java/test/websocket/service/WebServerService.java
+++ chatserver/src/main/java/test/websocket/service/WebServerService.java
@@ -5,20 +5,18 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import test.websocket.dto.CompanionChatDTO;
+import test.websocket.dto.ChatData;
 
 import java.util.List;
 
 @Service
 public class WebServerService {
-    public Mono<List<CompanionChatDTO>> getInCompleteNumbers() {
-        String url = "https://fb8e-1-237-37-135.ngrok-free.app/companions/incomplete-numbers";
+    public Mono<List<ChatData>> getInCompleteNumbers() {
+//        String url = "http://localhost:8080/companions/incomplete-numbers";
+        String url = "https://7860-221-140-143-39.ngrok-free.app/companions/incomplete-numbers";
         WebClient webClient = WebClient.create();
 
-        return webClient.get()
-                .uri(url)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<>() {});
+        return webClient.get().uri(url).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(new ParameterizedTypeReference<>() {
+        });
     }
 }
