5ecb3688ada03e0de1819cad27061553b3fd35fb
seungchul <seungchul0118@gmail.com>
2023-08-05 17:25:06 +0900

    :recycle: refactor : querydsl 이용 검색 기능 리팩토링

--- server/src/main/java/partypeople/server/companion/repository/CompanionRepository.java
+++ server/src/main/java/partypeople/server/companion/repository/CompanionRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import partypeople.server.companion.entity.Companion;
 
 import java.time.LocalDate;
